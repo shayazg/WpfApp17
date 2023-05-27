ae13249428a3deebe4089e85b0d1bae72423e31e
Azalia <azaliagg@yandex.ru>
2023-05-27 08:52:17 +0300

    Edit file MainWindow.xaml

--- WpfApp17/MainWindow.xaml.cs
+++ WpfApp17/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp17
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
